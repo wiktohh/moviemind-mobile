da9e673d61e2b2c87ca32c4d53efe29bafe09dd9
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-12-06 02:53:27 +0100

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,14 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./features/tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'movies/:id',
+    loadComponent: () => import('./features/movie-details/movie-details.page').then( m => m.MovieDetailsPage)
+  },
+  {
+    path: 'actors/:id',
+    loadComponent: () => import('./features/actor-details/actor-details.page').then( m => m.ActorDetailsPage)
+  },
+
+
 ];

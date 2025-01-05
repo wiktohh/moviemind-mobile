af552c2274d42db6d59687e7255ad3f218fa63c0
wiktohh <wiktor.rzeznicki01@gmail.com>
2025-01-05 14:53:02 +0100

    feat: implement watch list functionality with movie retrieval and detail navigation

--- src/app/features/watch-list/watch-list.page.ts
+++ src/app/features/watch-list/watch-list.page.ts
@@ -2,6 +2,9 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { MoviesServiceService } from 'src/app/shared/services/movies/movies.service';
+import { Genre, Movie } from 'src/app/shared/models/movies/movie.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-watch-list',
@@ -13,9 +16,26 @@ import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar 
 })
 export class WatchListPage implements OnInit {
 
-  constructor() { }
+  movies: Movie[] = [];
+  Genre = Genre;
+
+  constructor(private movieService: MoviesServiceService, private router: Router) { }
 
   ngOnInit() {
+    console.log("e uruchomisz sie?")
+    this.movieService.getWatchLater().subscribe({
+      next: (movies) => {
+        console.log(movies);
+        this.movies = movies;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
+  }
+
+  goToDetails(movieId: string) {
+    this.router.navigate(['/movies', movieId]); 
   }
 
 }

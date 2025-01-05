a6ef5d666bb4796ef7a047c39396173c95b6cbbc
wiktohh <wiktor.rzeznicki01@gmail.com>
2025-01-05 14:52:09 +0100

    feat: update auth guard to check for token presence instead of authentication status

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -5,7 +5,7 @@ import { AuthService } from 'src/app/shared/services/auth/auth.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  if(authService.isAuthenticated$) {
+  if(authService.getToken() !== null) {
     return true;
   } else {
     router.navigate(['/login']);

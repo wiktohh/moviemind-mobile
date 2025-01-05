373c0d2012305b6593a9b312e4df852a4e91e795
wiktohh <wiktor.rzeznicki01@gmail.com>
2025-01-05 14:52:51 +0100

    feat: enhance search functionality with debouncing and improved result handling

--- src/app/features/search/search.page.ts
+++ src/app/features/search/search.page.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Genre, Movie } from 'src/app/shared/models/movies/movie.model';
 import { MoviesServiceService } from 'src/app/shared/services/movies/movies.service';
@@ -6,7 +6,7 @@ import { IonHeader, IonToolbar, IonTitle, IonItem, IonContent, IonSearchbar, Ion
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { finalize } from 'rxjs';
+import { debounce, debounceTime, finalize, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -18,36 +18,49 @@ import { finalize } from 'rxjs';
             IonToolbar, CommonModule, FormsModule, TranslateModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class SearchPage implements OnInit {
+export class SearchPage implements OnInit, OnDestroy  {
   loading: boolean = false;
   searchTerm: string = '';
   movies: Movie[] = []
   Genre = Genre;
   filteredMovies: Movie[] = []
 
+  private searchSubject = new Subject<string>();
+  private destroy$ = new Subject<void>();
+
   constructor(private router: Router, private moviesService: MoviesServiceService) {}
 
-  ngOnInit() {
-    this.loading = true;
-    this.moviesService.getMovies().pipe(finalize(() => this.loading = false)).subscribe({
+  ngOnInit(): void {
+    this.searchSubject.pipe(
+      debounceTime(300),
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.onSearch();
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onSearch() {
+    if(this.searchTerm.length === 0) {
+      this.filteredMovies = [];
+      return;
+    }
+   this.loading = true;
+   this.moviesService.getMovieByTitle(this.searchTerm).pipe(finalize(() => this.loading = false)).subscribe({
       next: (movies: Movie[]) => {
-        this.movies = movies;
-        this.filteredMovies = [...this.movies];
+        this.filteredMovies = movies;
       },
       error: (error) => {
         console.log('Error:', error);
       }
     })
-    this.filteredMovies = [...this.movies];
-  }
-
-  onSearch() {
-    this.filteredMovies = this.movies.filter((movie) =>
-      movie.title.toLowerCase().includes(this.searchTerm.toLowerCase())
-    );
   }
 
   goToDetails(movieId: string) {
-    this.router.navigate(['/movies', movieId]); // Przejście do szczegółów filmu
+    this.router.navigate(['/movies', movieId]); 
   }
 }

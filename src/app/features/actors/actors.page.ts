28f78b5825fbef3805d3d543cac7ae0020ce1004
wiktohh <wiktor.rzeznicki01@gmail.com>
2025-01-05 15:42:20 +0100

    feat: enhance actors page with loading state and error handling during data retrieval

--- src/app/features/actors/actors.page.ts
+++ src/app/features/actors/actors.page.ts
@@ -3,6 +3,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonText, IonIcon, IonRow, IonButton, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonImg, IonCardContent, IonButtons } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
+import { finalize } from 'rxjs';
 import { Actor } from 'src/app/shared/models/actors/actor.model';
 import { ActorsService } from 'src/app/shared/services/actors/actors.service';
 
@@ -17,6 +18,7 @@ import { ActorsService } from 'src/app/shared/services/actors/actors.service';
 export class ActorsPage implements OnInit {
 
   actors: Actor[] = [];
+  loading: boolean = false
 
   constructor(
     private actorsService: ActorsService, 
@@ -24,7 +26,15 @@ export class ActorsPage implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.actors = this.actorsService.getActors();
+    this.loading = true
+    this.actorsService.getActors().pipe(finalize(() => this.loading = false)).subscribe({
+      next: (actors: Actor[]) => {
+        this.actors = actors
+      },
+      error: (error) => {
+        console.log('Error:', error)
+      }
+    })
   }
 
   onActorClick(actorId: string) {
